c1feb63b6b6560a727b8b774f6a23af82f31a09e
Camila Fonseca <camilarqf@gmail.com>
2024-06-05 11:31:50 -0300

    add prometheus

---
+++ src/main/java/http/response/PrometheusMetricsController.java
@@ -0,0 +1,26 @@
+package http.response;
+
+import io.prometheus.client.exporter.common.TextFormat;
+import io.prometheus.client.hotspot.DefaultExports;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class PrometheusMetricsController {
+
+    @PostConstruct
+    public void init() {
+        DefaultExports.initialize();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/prometheus")
+    public void scrape(HttpServletResponse response) throws IOException {
+        response.setContentType(TextFormat.CONTENT_TYPE_004);
+        TextFormat.write004(response.getWriter(), io.prometheus.client.CollectorRegistry.defaultRegistry.metricFamilySamples());
+    }
+}
